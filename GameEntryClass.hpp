6002e53fc6365c4ab5766d8915117e4a0fb6f5f5
Ashley Soderlund <69334327+Ashley-Soderlund@users.noreply.github.com>
2022-03-10 11:09:23 -0600

    Update GameEntryClass.hpp

--- GameEntryClass.hpp
+++ GameEntryClass.hpp
@@ -1,20 +1,25 @@
 #include <iostream>
+#include "Student.hpp"
 #pragma once
-using namespace std;
 
-class GameEntry {
+
+class GameEntry: public Student {
 public:
-    GameEntry(const string& n="", int s=0, int r=0);
-    string getName() const;
+    GameEntry(const std::string& n="", int s=0, int r=0);           //is there a better way to make a constructor that add Student member variables?
+    //GameEntry(Student s) : Student(s) {}
+    std::string getName() const;
     int getScore() const;
     int getRound() const;
-    void output(GameEntry& game) const;
+    std::string getSportName() const;
     void setScore( int score1);
     void setRound(int roundTotal);
-    ~GameEntry();
+    void setSportName(std::string sportName1);
+    friend void comparePlayers(GameEntry& game1, GameEntry& game2);                //compares two player objects based on their gpa and game score
+    friend void operator << (std::ostream& os, const GameEntry& game);             //overloading the '<<' operator to print out an object
+    //~GameEntry();
 private:
-    string name;
+    std::string sportName;
     int score;
     int roundTotal;
-
 };
+
