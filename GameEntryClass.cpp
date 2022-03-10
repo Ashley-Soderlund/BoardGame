93a229c82359ddda39f2827850989b9d38a29cab
Ashley Soderlund <69334327+Ashley-Soderlund@users.noreply.github.com>
2022-03-10 11:09:48 -0600

    Update GameEntryClass.cpp

--- GameEntryClass.cpp
+++ GameEntryClass.cpp
@@ -1,18 +1,13 @@
 #include <iostream>
 #include "GameEntry.hpp"
-using namespace std;
 
 
-void GameEntry::output(GameEntry& game) const{                        //outputs name and score of an object
-    cout << "The game name is: " << game.getName() << endl;
-    cout << "The game score is: " << game.getScore() << endl;
-}
+GameEntry::GameEntry(const std::string& n, int s, int r): sportName(n), score(s), roundTotal(r){}    //constructor
 
-GameEntry::GameEntry(const string& n, int s, int r): name(n), score(s), roundTotal(r){}    //constructor
-
-string GameEntry::getName() const {return name;}                    //gets the name paramater of an object
+std::string GameEntry::getName() const {return name;}                    //gets the name paramater of an object
 int GameEntry::getScore() const {return score;}                     //gets the score paramater of an object
 int GameEntry::getRound() const {return roundTotal;}                //gets the round paramater of an object
+std::string GameEntry:: getSportName() const {return sportName;}         //gets the sportName paramater of an object
 
 
 void GameEntry::setScore(int score1) {            //sets the score of an object
@@ -23,6 +18,39 @@ void GameEntry::setRound(int roundTotal1){        //sets the round total of an o
     roundTotal = roundTotal1;
 }
 
-GameEntry::~GameEntry(){       //Destructor
-    delete[] &name, score;
+void GameEntry::setSportName(std::string sportName1){  //sets the sport name value of an object
+    sportName = sportName1;
 }
+
+void comparePlayers(GameEntry& game1, GameEntry& game2){
+
+    std::cout << "Comparing two players based on their gpa and game score:" << std::endl;
+
+    if(game1.getSportName() == game2.getSportName() && game1.getSemester() == game2.getSemester()){
+        if(game1.getScore() >= game2.getScore() && game1.getGPA() >= game2.getGPA()){
+            std::cout << "The player " << game1.getName() << " is better than " << game2.getName() << std::endl;
+        }
+        else if (game1.getScore() <= game2.getScore() && game1.getGPA() <= game2.getGPA()){
+            std::cout << "The player " << game2.getName() << " is better than " << game1.getName() << std::endl;
+        }
+        else if(game1.getScore() == game2.getScore() && game1.getGPA() == game2.getGPA()){
+            std::cout << "The players " << game1.getName() << " and " << game2.getName() << " are equal." << std::endl;
+        }
+        else{
+            std::cout << "They are not equal and one player is not better in both the categories of gpa and score." << std::endl;
+        }
+    }
+    else{
+        std::cout << "These players cannot be compared because their semester and sport are not the same." << std::endl;
+    }
+    std::cout << std::endl;
+};
+
+void operator << (std::ostream& os, const GameEntry& game){
+        std::cout << game.getName() << std::endl;
+}
+
+//GameEntry::~GameEntry(){       //Destructor
+  //  delete[] &name, score;
+//}
+
