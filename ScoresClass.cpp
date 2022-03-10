5837a7a09e4f8cecc5a55e8453d0ad9f199e9393
Ashley Soderlund <69334327+Ashley-Soderlund@users.noreply.github.com>
2022-03-10 11:11:05 -0600

    Update ScoresClass.cpp

--- ScoresClass.cpp
+++ ScoresClass.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include "Scores.hpp"
-using namespace std;
 
 
 Scores::Scores(int maxEnt){                           //constuctor 
@@ -14,9 +13,10 @@ Scores::~Scores(){                                    //destructor
 }
 
 void Scores::output(const GameEntry& pl1, const GameEntry& pl2, const GameEntry& pl3){             //output round totals of each player
-    cout << pl1.getName() << ": " << pl1.getRound() << endl;
-    cout << pl2.getName() << ": " << pl2.getRound() << endl;
-    cout << pl3.getName() << ": " << pl3.getRound() << endl;
+    std::cout << pl1.getName() << ": " << pl1.getRound() << std::endl;
+    std::cout << pl2.getName() << ": " << pl2.getRound() << std::endl;
+    std::cout << pl3.getName() << ": " << pl3.getRound() << std::endl;
+    std:cout << std::endl;
 }
 
 void Scores::add(const GameEntry& e){                                                              //add an entry to object
