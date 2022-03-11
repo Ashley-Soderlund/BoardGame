908652bb03240a6cac711a51c0a75cc7b3c78757
Ashley Soderlund <69334327+Ashley-Soderlund@users.noreply.github.com>
2022-03-11 16:32:47 -0600

    Update and rename ScoresClass.hpp to Score.hpp

---
+++ Score.hpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#pragma once
+
+class Student{
+    public:
+        Student(const std::string& n="", float g=0.0, int s=0);
+        std::string getName() const;
+        float getGPA() const;
+        int getSemester() const;
+        void setName(std::string valueName);
+        void setGPA(float valueGPA);
+        void setSemester(int valueSemester);
+        //~Student();
+        std::string name {};
+        float gpa {};
+        int semester {};
+};
